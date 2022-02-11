d913356f92b98ae84fc514d3a6651ef9c660dd57
DESKTOP-N6P7QS1\isp40 <kwonjoondong@gmail.com>
2022-02-12 00:05:59 +0900

    [CHORE] DTO 삭제 (GET 요청은 DTO가 필요없다.)

--- app/src/webrtc-mediator/webrtc-mediator.gateway.ts
+++ app/src/webrtc-mediator/webrtc-mediator.gateway.ts
@@ -6,37 +6,42 @@ import {
     OnGatewayConnection,
     OnGatewayDisconnect
 } from '@nestjs/websockets';
-import {Socket, Server} from 'socket.io';
+import {Socket as OriginSocket, Server} from 'socket.io';
 import {Logger} from '@nestjs/common';
 
-@WebSocketGateway({
-    namespace: 'webrtc/socket'
-})
+interface Socket extends OriginSocket {
+    nickname : string;
+}
+
+@WebSocketGateway({namespace: 'webrtc/socket'})
 export class WebrtcMediatorGateway implements OnGatewayInit,
 OnGatewayConnection,
 OnGatewayDisconnect {
+    sockets : Socket[];
+
     private logger : Logger = new Logger('WebrtcMediatorGateway');
     @WebSocketServer()
     server : Server;
+
     // 게이트웨이가 초기화되고 나서 실행
     afterInit(server : any) {
-        this
-            .logger
-            .log('Initialize WebrtcMediatorGateway!');
+        this.logger.log('Initialize WebrtcMediatorGateway!');
     }
 
     handleConnection(client : Socket, ...args : any[]) {
-        this
-            .logger
-            .log(`Client connected: ${client.id}`);
+
+        client.nickname = client.handshake.query.nickname as string;
+        this.logger.log(`Client connected: ${client.handshake.query.nickname}`);
+        return 'hello';
     }
 
     handleDisconnect(client : Socket) {
-        this
-            .logger
-            .log(`Client disconnected: ${client.id}`);
+        this.logger .log(`Client disconnected: ${client.id}`);
     }
 
     @SubscribeMessage('message')
-    handleMessage(client : any, payload : any) : string {return 'Hello world!';}
+    handleMessage(client : any, payload : any) {
+        this.logger.log("message", client.nickname, payload);
+        client.emit('welcome', {message: 'welcome'});
+    }
 }
